085af7b949d525e5dd04009a5d95a9216bb708f1
Roman Batygin <rbatygin@igatec.com>
2026-09-23 15:12:57 +0700

    added check spt paths in unit tests

--- src/test/java/graphcollection/algorithms/shortestpaths/BellmanFordShortestPathsTest.java
+++ src/test/java/graphcollection/algorithms/shortestpaths/BellmanFordShortestPathsTest.java
@@ -5,6 +5,8 @@ import graphcollection.gui.model.Edge2D;
 import graphcollection.gui.model.Vertex;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static graphcollection.TestHelperUtils.readGraph;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,10 +17,20 @@ class BellmanFordShortestPathsTest {
         // example from page 673 Kormen_Tomas_Algoritmy_postroenie_i_analiz
         Graph<Vertex, Edge2D> graph = readGraph("bellman_ford_spt.json");
         Vertex source = new Vertex("s");
-        BellmanFordShortestPaths<Vertex, Edge2D> dijkstraShortestPaths = new BellmanFordShortestPaths<>(graph, source);
-        assertEquals(2d, dijkstraShortestPaths.getDistance(new Vertex("t")).doubleValue());
-        assertEquals(7d, dijkstraShortestPaths.getDistance(new Vertex("y")).doubleValue());
-        assertEquals(4d, dijkstraShortestPaths.getDistance(new Vertex("x")).doubleValue());
-        assertEquals(-2d, dijkstraShortestPaths.getDistance(new Vertex("z")).doubleValue());
+        Vertex t = new Vertex("t");
+        Vertex x = new Vertex("x");
+        Vertex y = new Vertex("y");
+        Vertex z = new Vertex("z");
+        BellmanFordShortestPaths<Vertex, Edge2D> shortestPaths = new BellmanFordShortestPaths<>(graph, source);
+        assertEquals(2d, shortestPaths.getDistance(t).doubleValue());
+        assertEquals(7d, shortestPaths.getDistance(y).doubleValue());
+        assertEquals(4d, shortestPaths.getDistance(x).doubleValue());
+        assertEquals(-2d, shortestPaths.getDistance(z).doubleValue());
+
+        assertEquals(List.of(source), shortestPaths.getPath(source));
+        assertEquals(List.of(source, y), shortestPaths.getPath(y));
+        assertEquals(List.of(source, y, x), shortestPaths.getPath(x));
+        assertEquals(List.of(source, y, x, t), shortestPaths.getPath(t));
+        assertEquals(List.of(source, y, x, t, z), shortestPaths.getPath(z));
     }
 }
--- src/test/java/graphcollection/algorithms/shortestpaths/DijkstraShortestPathsTest.java
+++ src/test/java/graphcollection/algorithms/shortestpaths/DijkstraShortestPathsTest.java
@@ -5,6 +5,8 @@ import graphcollection.gui.model.Edge2D;
 import graphcollection.gui.model.Vertex;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static graphcollection.TestHelperUtils.readGraph;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,11 +16,20 @@ class DijkstraShortestPathsTest {
     void testShortestPaths() {
         // example from page 682 Kormen_Tomas_Algoritmy_postroenie_i_analiz
         Graph<Vertex, Edge2D> graph = readGraph("dijkstra_spt.json");
-        Vertex source = new Vertex("s");
-        DijkstraShortestPaths<Vertex, Edge2D> dijkstraShortestPaths = new DijkstraShortestPaths<>(graph, source);
-        assertEquals(8d, dijkstraShortestPaths.getDistance(new Vertex("t")).doubleValue());
-        assertEquals(5d, dijkstraShortestPaths.getDistance(new Vertex("y")).doubleValue());
-        assertEquals(9d, dijkstraShortestPaths.getDistance(new Vertex("x")).doubleValue());
-        assertEquals(7d, dijkstraShortestPaths.getDistance(new Vertex("z")).doubleValue());
+        Vertex s = new Vertex("s");
+        Vertex t = new Vertex("t");
+        Vertex x = new Vertex("x");
+        Vertex y = new Vertex("y");
+        Vertex z = new Vertex("z");
+        DijkstraShortestPaths<Vertex, Edge2D> dijkstraShortestPaths = new DijkstraShortestPaths<>(graph, s);
+        assertEquals(8d, dijkstraShortestPaths.getDistance(t).doubleValue());
+        assertEquals(5d, dijkstraShortestPaths.getDistance(y).doubleValue());
+        assertEquals(9d, dijkstraShortestPaths.getDistance(x).doubleValue());
+        assertEquals(7d, dijkstraShortestPaths.getDistance(z).doubleValue());
+
+        assertEquals(List.of(s, y), dijkstraShortestPaths.getPath(y));
+        assertEquals(List.of(s, y, z), dijkstraShortestPaths.getPath(z));
+        assertEquals(List.of(s, y, t), dijkstraShortestPaths.getPath(t));
+        assertEquals(List.of(s, y, t, x), dijkstraShortestPaths.getPath(x));
     }
 }
